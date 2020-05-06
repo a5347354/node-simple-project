d8ee058df577cb6e57043c2f377098b987820df3
Luke <a5347354@gmail.com>
2020-05-06 15:42:23 +0800

    Add http_get

---
+++ http/http_get.js
@@ -0,0 +1,80 @@
+async function resuq(){
+    const http = require('http')
+      await http.get('http://localhost:8080/site/line', (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
+    
+        let error;
+        if (statusCode !== 200) {
+          error = new Error('Request Failed.\n' +
+                            `Status Code: ${statusCode}`);
+        } else if (!/^application\/json/.test(contentType)) {
+          error = new Error('Invalid content-type.\n' +
+                            `Expected application/json but received ${contentType}`);
+        }
+        if (error) {
+          console.error(error.message);
+          // Consume response data to free up memory
+          res.resume();
+          return;
+        }
+    
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => { rawData += chunk; });
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            console.log(parsedData);
+          } catch (e) {
+            console.error(e.message);
+          }
+        });
+      }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+      });
+    }
+    module.exports = resuq;
+    
+    
+    
+    
+    
+    
+    // // http://localhost:8080/site/line
+    // async function resqu() {
+    //   const https = require('http')
+    //   const options = {
+    //     hostname: 'localhost',
+    //     port: 8080,
+    //     path: '/site/line',
+    //     method: 'GET'
+    //   }
+    //
+    //   const req = await https.request(options, res => {
+    //     console.log(`statusCode: ${res.statusCode}`)
+    //     var body = '';
+    //
+    //     res.on('data', d => {
+    //       process.stdout.write(d)
+    //       body += d;
+    //       console.log(d)
+    //       // return d;
+    //     })
+    //   })
+    //
+    //   req.on('end', fbResponse=>{
+    //   fbResponse = JSON.parse(body);
+    //
+    //   })
+    //
+    //   req.on('error', error => {
+    //     console.error(error);
+    //   })
+    //
+    //   req.end();
+    //   return fbResponse;
+    // }
+    //
+    // exports.resqu = resqu;
+    
